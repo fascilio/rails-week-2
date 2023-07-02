89dce90f62be53269f46e8a702cbbe969323335d
fascilio <fascilior@gmail.com>
2023-07-02 13:36:50 +0300

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'powers/index'
+  get 'powers/show'
+  get 'powers/update'
+  get 'heroes/index'
+  get 'heroes/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
